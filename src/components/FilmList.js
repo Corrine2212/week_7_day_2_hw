2dcd0e7832567992b130089aeea4c9fa307ccc4b
Corrine Sing <corrine.sing@gmail.com>
2023-05-02 21:34:41 +0100

    first commit

---
+++ src/components/FilmList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Film from './Film';
+
+const FilmList = ({films}) => {
+
+    const filmNodes = films.map((film) => {
+        return <Film key={film.id} film={film} />
+    })
+
+    return (
+        <>
+        {filmNodes}
+        </>
+    );
+
+}
+
+export default FilmList;
